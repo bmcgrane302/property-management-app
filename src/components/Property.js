2e8c04ec24b4de99e3214bde61b39ec60937150e
bmcgrane302 <bmcgrane302@yahoo.com>
2017-12-13 14:56:53 -0700

    style property card

--- src/components/Property.js
+++ src/components/Property.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { removeProperty } from '../actions/properties';
 import { bindActionCreators } from 'redux';
-import { Card, CardImg, CardText, CardBody,
+import { Card, CardImg, CardText, CardBody, Row, Col,
   CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 
@@ -11,29 +11,116 @@ class Property extends Component {
   render () {
     return (
       <div>
-        <Card>
+        <Card style={{marginTop: 25}}>
         <CardBody className="text-left">
-          <CardTitle>{this.props.property.address}  unit: {this.props.property.unit}</CardTitle>
-          <CardSubtitle>{this.props.property.city} {this.props.property.state} {this.props.property.zipcode} </CardSubtitle>
-          <CardSubtitle>Tenant: {this.props.property.fname} {this.props.property.lname}</CardSubtitle>
-          <CardSubtitle>Mortgage: ${this.props.property.mortgage}</CardSubtitle>
-          <CardSubtitle>Property Tax: ${this.props.property.property_tax}</CardSubtitle>
-          <CardSubtitle>Property Insurance: ${this.props.property.property_insurance}</CardSubtitle>
-          <CardSubtitle>Rent: ${this.props.property.rent_amount}</CardSubtitle>
-          <CardSubtitle>Lease start date: {this.props.property.lease_start_date}</CardSubtitle>
-          <CardSubtitle>Lease end date: {this.props.property.lease_end_date}</CardSubtitle>
-          <CardSubtitle>Rent status: {this.props.property.rent_paid}</CardSubtitle>
-          <CardSubtitle>Repairs: {this.props.property.repairs}</CardSubtitle>
-          <CardSubtitle>Repair comments: {this.props.property.repair_description}</CardSubtitle>
-
-
+          <Row  >
+            <Col>
+              <CardTitle>{this.props.property.address} </CardTitle>
+            </Col>
+            <Col>
+              <CardTitle>Unit: {this.props.property.unit}</CardTitle>
+            </Col>
+            <Col>
+              <CardTitle>{this.props.property.city} {this.props.property.state} {this.props.property.zipcode} </CardTitle>
+            </Col>
+          </Row>
+          <Row style={{paddingBottom: 10}}>
+            <Col sm='4'>
+              <CardSubtitle>Tenant</CardSubtitle>
+            </Col>
+            <Col sm='8'>
+              <CardSubtitle>{this.props.property.fname} {this.props.property.lname}</CardSubtitle>
+            </Col>
+          </Row>
+          <Row style={{paddingBottom: 10}}>
+            <Col sm='4'>
+              <CardSubtitle>Mortgage: </CardSubtitle>
+            </Col>
+            <Col sm='8'>
+              <CardSubtitle>${this.props.property.mortgage}</CardSubtitle>
+            </Col>
+          </Row>
+          <Row style={{paddingBottom: 10}}>
+            <Col sm='4'>
+              <CardSubtitle>Property Tax: </CardSubtitle>
+            </Col>
+            <Col sm='8'>
+              <CardSubtitle>${this.props.property.property_tax}</CardSubtitle>
+            </Col>
+          </Row>
+          <Row style={{paddingBottom: 10}}>
+            <Col sm='4'>
+              <CardSubtitle>Property Tax: </CardSubtitle>
+            </Col>
+            <Col sm='8'>
+              <CardSubtitle>${this.props.property.property_tax}</CardSubtitle>
+            </Col>
+          </Row>
+          <Row style={{paddingBottom: 10}}>
+            <Col sm='4'>
+              <CardSubtitle>Property Insurance:</CardSubtitle>
+            </Col>
+            <Col sm='8'>
+              <CardSubtitle>${this.props.property.property_insurance}</CardSubtitle>
+            </Col>
+          </Row>
+          <Row style={{paddingBottom: 10}}>
+            <Col sm='4'>
+              <CardSubtitle>Rent:</CardSubtitle>
+            </Col>
+            <Col sm='8'>
+              <CardSubtitle>${this.props.property.rent_amount}</CardSubtitle>
+            </Col>
+          </Row>
+          <Row style={{paddingBottom: 10}}>
+            <Col sm='4'>
+              <CardSubtitle>Lease start date:</CardSubtitle>
+            </Col>
+            <Col sm='8'>
+              <CardSubtitle>{this.props.property.lease_start_date}</CardSubtitle>
+            </Col>
+          </Row>
+          <Row style={{paddingBottom: 10}}>
+            <Col sm='4'>
+              <CardSubtitle>Lease end date: </CardSubtitle>
+            </Col>
+            <Col sm='8'>
+              <CardSubtitle>{this.props.property.lease_end_date}</CardSubtitle>
+            </Col>
+          </Row>
+          <Row style={{paddingBottom: 10}}>
+            <Col sm='4'>
+             <CardSubtitle>Rent status:</CardSubtitle>
+            </Col>
+            <Col sm='8'>
+              {this.props.property.rent_paid?<CardSubtitle style={{color: 'green' }}>PAID</CardSubtitle> :
+              <CardSubtitle style={{color: 'red' }}>UNPAID</CardSubtitle>}
+            </Col>
+          </Row>
+          <Row style={{paddingBottom: 10}}>
+            <Col sm='4'>
+            <CardSubtitle>Repairs:</CardSubtitle>
+            </Col>
+            <Col sm='8'>
+              {this.props.property.repairs?<CardSubtitle >NONE</CardSubtitle> :
+              <CardSubtitle style={{color: 'red' }}>NEEDED</CardSubtitle>}
+            </Col>
+          </Row>
+          <Row style={{paddingBottom: 10}}>
+            <Col sm='4'>
+            <CardSubtitle>Repair comments: </CardSubtitle>
+            </Col>
+            <Col sm='8'>
+              <CardSubtitle>{this.props.property.repair_description}</CardSubtitle>
+            </Col>
+          </Row>
             <Button
               onClick={()=> this.props.removeProperty(this.props.property.id)}
             >remove
           </Button>
         </CardBody>
       </Card>
-      </div>
+    </div>
     )
   }
 }
